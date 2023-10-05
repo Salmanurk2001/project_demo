8391ff784fb7295c7d7ec19bb3d468637b39dcc9
Salmanurk2001 <salmanurk2001@gmail.com>
2023-10-05 13:07:05 +0530

    Initial commit

--- bin/list ex.dart
+++ bin/list ex.dart
@@ -1,12 +1,28 @@
 void main(){
-  var list1=[];
-  var list2=[1]
-  var list2=[1,2,3,4,5,6,7,8];
+
+  var list1 = [];  // empty list
+  var list2 = [1,2,3,4,5,"hello"];  // dynamic list
+  List<int> list3 = [1,2,3,4,5,6,7,8];
+  print(list1);
   print(list2);
   print(list3);
   list3.add(10);
   print(list3);
   list3.addAll([11,12,13]);
   print(list3);
-  print(list3.)
+  print(list3.length);
+  print(list3[10]);   //listname[index]
+  print(list3.contains(10));
+  list3.insert(2, 100);
+  print(list3);
+  print(list1.isEmpty);
+
+  for(int index=0 ; index < list3.length ; index++){
+    print('list3 [$index] = ${list3[index]}');
+  }
+
+  for(var i in list3){
+    print(i);
+  }
+
 }
