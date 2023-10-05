73d748f5efc1e4b6932d57b054e4576b957e4dc3
Salmanurk2001 <salmanurk2001@gmail.com>
2023-10-05 12:48:22 +0530

    Initial commit

---
+++ bin/list ex.dart
@@ -0,0 +1,12 @@
+void main(){
+  var list1=[];
+  var list2=[1]
+  var list2=[1,2,3,4,5,6,7,8];
+  print(list2);
+  print(list3);
+  list3.add(10);
+  print(list3);
+  list3.addAll([11,12,13]);
+  print(list3);
+  print(list3.)
+}
